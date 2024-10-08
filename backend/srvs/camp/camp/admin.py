7bf73fa021e787c09c3f007c3e7f9d8bfd553e6e
Amin Bidad <amin.200.bidad@gmail.com>
2024-10-08 08:04:06 +0330

    Add initial Company model and view (#2)

--- backend/srvs/camp/camp/admin.py
+++ backend/srvs/camp/camp/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from backend.srvs.camp.camp.models import Post
+from backend.srvs.camp.camp.models import (
+    Post,
+    CompanyPage,
+)
 
 admin.site.register(Post)
+admin.site.register(CompanyPage)
--- backend/srvs/camp/camp/views.py
+++ backend/srvs/camp/camp/views.py
@@ -1,9 +1,21 @@
-from backend.srvs.camp.camp.models import Post
-from backend.srvs.camp.camp.serializers import PostSerializer
+from backend.srvs.camp.camp.models import (
+    Post,
+    CompanyPage,
+)
+from backend.srvs.camp.camp.serializers import (
+    PostSerializer,
+    CompanyPageSerializer,
+)
 from rest_framework.viewsets import ModelViewSet
 
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    ordering = ["-created_at"]
+
+
+class CompanyPageViewSet(ModelViewSet):
+    queryset = CompanyPage.objects.all()
+    serializer_class = CompanyPageSerializer
     ordering = ["-created_at"]
